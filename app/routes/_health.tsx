1ee2e366f985a9f4f58774d166f36c8ca998f744
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-07-17 20:37:17 +0200

    ci: add render related stuff

---
+++ app/routes/_health.tsx
@@ -0,0 +1,19 @@
+import type { LoaderArgs } from '@remix-run/node';
+
+const fetchSelf = async (request: Request) => {
+	const host = request.headers.get('host');
+	const selfURL = new URL('/', `http://${host}`);
+	const response = await fetch(selfURL.toString(), { method: 'HEAD' });
+
+	if (!response.ok) throw new Error('Self head fetch failed');
+};
+
+export const loader = async ({ request }: LoaderArgs) => {
+	try {
+		await fetchSelf(request);
+
+		return new Response('OK', { status: 204 });
+	} catch {
+		return new Response('FAIL', { status: 500 });
+	}
+};
